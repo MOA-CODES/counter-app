0055da4398e17b30460f168eb4d03de40bb8947e
MOA -CODES <moyo.ajla@gmail.com>
2022-12-20 18:52:56 +0100

    1st

---
+++ src/components/counter.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+   
+        state={
+            count: 0
+        };
+
+        render() { 
+        return(
+             <React.Fragment>
+                <span>{this.formatCount()}</span>
+                <button>Increment</button>
+                </React.Fragment>
+        );//this is a jsx component (must have one parent)
+        // 'this' references the object we are currenty working with
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
