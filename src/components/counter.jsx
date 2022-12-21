f3f85f550b6312dd08c3ccce5ea7584979a20fa8
MOA -CODES <moyo.ajla@gmail.com>
2022-12-21 21:20:26 +0100

    3rd

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,56 @@ import React, { Component } from 'react';
 class Counter extends Component {
    
         state={
-            count: 0
+            count: 0,
+            tags: [],
+        };
+
+        styles={
+            fontSize: 15,
+            fontWeight: 'bold',
         };
 
         render() { 
         return(
              <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-                </React.Fragment>
-        );//this is a jsx component (must have one parent)
-        // 'this' references the object we are currenty working with
-    }
-//jsx expression's get compiled to react elements
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                 onClick={this.handleIncrement }
+                 style= {{fontSize : 15}}
+                 className='btn btn-secondary btn-sm'>Increment</button>
 
+                {/* {this.state.tags.length === 0 && "please create a new Tag"} */}
+                {/* or *
+                 {this.state.tags. === 0? "please create a new Tag" : ''} 
+                 {this.renderTags()} */}
+
+                </React.Fragment>
+        );
+        //this is a jsx component (must have one parent)
+        // 'this' references the object we are currenty working with
+        //jsx expression's get compiled to react elements
+    }
+
+    handleIncrement = () =>{ 
+        this.setState({ count: this.state.count + 1})
+     }
+
+    renderTags(){
+        if(this.state.tags.length === 0){
+            return <p>There are no tags</p>
+        }
+        
+       return(
+        //   taking a string and mapping it to a jsx object 
+       <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
+       );
+    }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
     formatCount(){
         const {count} = this.state;
         return count === 0? 'Zero' : count;
