4c8d33e8b36b286ddeb5cc1f59670c74485f1616
MOA -CODES <moyo.ajla@gmail.com>
2022-12-24 15:10:26 +0100

    5th

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
    
         state={
-            count: 0,
+            value: this.props.value === undefined? 0: this.props.value,
             tags: [],
         };
 
@@ -14,29 +14,41 @@ class Counter extends Component {
 
         render() { 
         return(
-             <React.Fragment>
+             <div>
+                {this.props.children}
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button
-                 onClick={this.handleIncrement }
+                //  onClick={() => this.handleIncrement(Counter.product) }
+                 onClick={() => this.handleIncrement2() }
+
                  style= {{fontSize : 15}}
                  className='btn btn-secondary btn-sm'>Increment</button>
+
+                 <button onClick={() => this.handleDecrement()} className="btn btn-danger btn-sm m-2"> - </button>
+                 <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2"> Delete </button>
+
 
                 {/* {this.state.tags.length === 0 && "please create a new Tag"} */}
                 {/* or *
                  {this.state.tags. === 0? "please create a new Tag" : ''} 
                  {this.renderTags()} */}
-
-                </React.Fragment>
+                </div>
         );
         /*this is a jsx component (must have one parent)
         // 'this' references the object we are currenty working with
         //jsx expression's get compiled to react elements*/
     }
 
-    handleIncrement = () =>{ 
-        this.setState({ count: this.state.count + 1})
+    handleIncrement = (product) =>{
+        console.log(product); 
+        this.setState({ value: this.state.value + 1})
      }
-
+     handleIncrement2 = () =>{
+        this.setState({ value: this.state.value + 1})
+     }
+     handleDecrement = () =>{
+        this.setState({ value: this.state.value - 1})
+     }
     renderTags(){
         if(this.state.tags.length === 0){
             return <p>There are no tags</p>
@@ -44,17 +56,17 @@ class Counter extends Component {
         
        return(
         //   taking a string and mapping it to a jsx object 
-       <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>
+       <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)} </ul>
        );
     }
 
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
     formatCount(){
-        const {count} = this.state;
+        const {value: count} = this.state;
         return count === 0? 'Zero' : count;
     }
 }
